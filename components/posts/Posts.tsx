cb3bbbbb2e8cfb730fccec3b6fc0c1e00f73d9d3
Deborah Lyra <deborahp.lyra@gmail.com>
2024-05-23 16:10:37 -0300

    estilo do post

--- components/posts/Posts.tsx
+++ components/posts/Posts.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Box, Center, Flex, Heading, Text } from 'native-base';
+import { Box, Center, Flex, Heading, Text, IconButton, Icon } from 'native-base';
 import { Avatar } from '../avatar/Avatar';
+import { MaterialIcons } from '@expo/vector-icons'; // Importando ícones do MaterialIcons
 
 interface PostProps {
   content: {
@@ -11,28 +12,72 @@ interface PostProps {
       role: string;
       avatarUrl: string;
     };
-    postContent: 'conteúdo do post',
-    publishedAt: "2024-04-30T20:20:00.000Z",
+    postText: string;
+    publishedAt: string;
   };
 }
 
 export function Posts({ content }: PostProps) {
   return (
-
     <Box mt={32} px={4}>
       <Center>
         <Heading mb={10} fontSize="3xl" color="tertiary.600">ForumTalk</Heading>
       </Center>
-      <Box bg= 'muted.400' p={4} borderRadius={5}>
-        <Flex direction="row" alignItems="center" justifyContent="start" >
+      <Box bg='muted.400' p={4} borderRadius={5}>
+        <Flex direction="row" alignItems="center" justifyContent="start">
           <Avatar src={content.author.avatarUrl} />
           <Box ml={4}>
             <Text fontSize="md" bold>{content.author.name}</Text>
             <Text fontSize="xs" bold>{content.author.role}</Text>
           </Box>
         </Flex>
+        <Box bg='muted.500' p={4} borderRadius={5} mt={5}>
+          <Flex direction="row" justifyContent="space-between" alignItems="center">
+            <Box maxW={'80%'}>
+              <Text fontSize="sm" color='muted.900' bold>{content.postText}</Text>
+            </Box>
+            <IconButton
+              icon={<Icon as={MaterialIcons} name="delete" size="5" />}
+              borderRadius="full"
+              _icon={{
+                color: "red.500",
+                size: "lg",
+              }}
+              _hover={{
+                bg: "red.600:alpha.20",
+              }}
+              _pressed={{
+                bg: "red.600:alpha.20",
+              }}
+              onPress={() => console.log('Delete post', content.id)}
+            />
+          </Flex>
+
+          <Flex direction="row">
+            <IconButton
+              icon={<Icon as={MaterialIcons} name="favorite" size="5" />}
+              borderRadius="full"
+              w={10}
+              _icon={{
+                color: "red.50",
+                size: "lg",
+              }}
+              onPress={() => console.log('like post', content.id)}
+            />
+            <IconButton
+              icon={<Icon as={MaterialIcons} name="keyboard-arrow-down" size="5" />}
+              borderRadius="full"
+              w={10}
+
+              _icon={{
+                color: "tertiary.600",
+                size: "lg",
+              }}
+              onPress={() => console.log('show post', content.id)}
+            />
+          </Flex>
+        </Box>
       </Box>
     </Box>
-
   );
 }
--- screens/dashboard/Dashboard.tsx
+++ screens/dashboard/Dashboard.tsx
@@ -8,7 +8,9 @@ const post = {
     name: "The Worried Pug",
     role: "Web Developer",
     avatarUrl: "https://images.unsplash.com/photo-1518020382113-a7e8fc38eac9?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDF8fHxlbnwwfHx8fHw%3D"
-  }
+  },
+  postText: "texto do post texto do posttexto do posttexto do posttexto do posttexto do posttexto do post ",
+    publishedAt: "2024-04-30T20:20:00.000Z"
 };
 
 
